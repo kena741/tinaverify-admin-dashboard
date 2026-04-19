c0cac95042d193c8ec594fe220a5a523c32e7670
Admas Terefe Girma <admasterefe00@gmail.com>
2026-04-19 17:51:07 +0300

    fix: provide default values for Supabase URL and anon key

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL! || "https://tovvoeffgngoxcvjoktu.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvdnZvZWZmZ25nb3hjdmpva3R1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzMjA3NTYsImV4cCI6MjA4MTg5Njc1Nn0.R7b1oiiScdwHqULwIu39dHIR0jF17Pme5RTeFUkO3m4";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
